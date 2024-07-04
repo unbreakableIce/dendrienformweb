4c4a1fc784dc1b241e0bc40f875c49bf42f98d58
Terrence <4597837+unbreakableIce@users.noreply.github.com>
2024-07-04 14:56:11 -0500

    updated redis connection to azure

--- app/utils/connection.ts
+++ app/utils/connection.ts
@@ -1,8 +1,13 @@
 import Redis from "ioredis";
 
+// const redis = new Redis(
+// 	"redis://default:4056d9bcb7f246b5ab89e3871a7ae260@us1-cunning-cougar-38621.upstash.io:38621"
+// );
+
 const redis = new Redis(
-	"redis://default:4056d9bcb7f246b5ab89e3871a7ae260@us1-cunning-cougar-38621.upstash.io:38621"
-);
+	"redis://default:MGZCyhmhg40Iz5ibLztRsHoZhDoRk89HYAzCaChVBR0=@dendrien-dev.redis.cache.windows.net:6380"
+ );
+
 // const redis = new Redis({
 // 	port: 6380, // Redis port
 // 	host: "127.0.0.1", // Redis host
