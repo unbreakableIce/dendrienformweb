1c8b5ea1fd0f76c107639dbd0351348c7e5f74df
Terrence <4597837+unbreakableIce@users.noreply.github.com>
2024-07-19 16:16:41 -0500

    removed openai key

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,11 +1,1 @@
 export const BACKEND_URL = "http://localhost:5227";
-// TODO remove this secret key
-export const OPENAI_API_KEY =
-	"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";
-
-// chat gpt
-//UN: tech@dendrien.com
-//PW: HelloTech#13a
-//
-//
-//
